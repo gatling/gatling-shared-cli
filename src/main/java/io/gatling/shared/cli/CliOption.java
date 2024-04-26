c182e889ad0002deb5f8ae79c4dd5d2a4f5607b2
slandelle <slandelle@gatling.io>
2024-04-26 13:52:50 +0200

    feat: revamp names and add options

--- src/main/java/io/gatling/shared/cli/CliOption.java
+++ src/main/java/io/gatling/shared/cli/CliOption.java
@@ -21,16 +21,27 @@ import static java.util.Objects.requireNonNull;
 import edu.umd.cs.findbugs.annotations.NonNull;
 
 public final class CliOption {
-  public final String full;
-  public final String abbr;
+  public final String fullName;
+  public final String abbreviatedName;
   public final String text;
   public final String valueName;
 
   public CliOption(
-      @NonNull String full, @NonNull String abbr, @NonNull String text, String valueName) {
-    this.full = requireNonNull(full);
-    this.abbr = requireNonNull(abbr);
+      @NonNull String fullName,
+      @NonNull String abbreviatedName,
+      @NonNull String text,
+      String valueName) {
+    this.fullName = requireNonNull(fullName);
+    this.abbreviatedName = requireNonNull(abbreviatedName);
     this.text = requireNonNull(text);
     this.valueName = valueName;
+  }
+
+  public String fullOption() {
+    return "--" + fullName;
+  }
+
+  public String abbreviatedOption() {
+    return "-" + abbreviatedName;
   }
 }
